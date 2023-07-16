122bab073b5522e48904cb5083a16bc6734ae591
Jules K <89418218+julesk1702@users.noreply.github.com>
2023-07-16 14:51:07 +0200

    Add files via upload

---
+++ api/01. DROP TABLE script.sql
@@ -0,0 +1,15 @@
+use `quintor-main`;
+
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE diets;
+DROP TABLE event_feedback;
+DROP TABLE events;
+DROP TABLE guest_diets;
+DROP TABLE guests;
+DROP TABLE ideas;
+DROP TABLE registrations;
+DROP TABLE user_custom_diets;
+DROP TABLE user_diets;
+DROP TABLE user_likes_ideas;
+DROP TABLE users;
+SET FOREIGN_KEY_CHECKS=1;
